a5348e0ba2326b159159541152f71ba73531f5ab
kyewu <18336768355@163.com>
2024-03-17 18:57:26 +0800

    feat: add unocss, preset tailwind css, add sass

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
   </header> -->
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <!-- <RouterView /> -->
+  <div class="btn-green">
+    234
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4210
   },
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [vue(), vueJsx(), VueDevTools(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
