6cf1e8242d6cbade7f1e6a9d72c1a78612935089
kyewu <18336768355@163.com>
2024-03-17 17:14:06 +0800

    feat: add tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  prefix: 'tw-',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    {
+      'postcss-import': {},
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  ]
+}
