8ab13b8d8fdb81f61c92f563f3522f624f26a037
kyewu <18336768355@163.com>
2024-03-23 20:40:07 +0800

    feat: config vue macros

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueMacros from 'unplugin-vue-macros/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -21,7 +22,11 @@ export default defineConfig({
       extensions: ['.vue', '.md'],
       dts: 'src/typed-router.d.ts',
     }), // VueRouter must be placed before Vue
-    vue(),
+    VueMacros({
+      plugins: {
+        vue: vue(),
+      },
+    }),
     Pages({
       dirs: [
         { dir: 'src/pages', baseRoute: '' },
